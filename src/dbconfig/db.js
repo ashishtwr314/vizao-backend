e8baee21da08cfbc311dec73977f98a7a6bdee0c
Ashish Tiwari <ashish.tiwari@KAYADEV.COM>
2021-04-28 21:07:10 +0530

    Change in DB creds

--- src/dbconfig/db.js
+++ src/dbconfig/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require("sequelize");
 
 module.exports = new Sequelize(
-  "dd7oskvf6vuls3",
-  "zmrcxwsomkcmhe",
-  "14900c88b9a37ea765cab9d97e19dcb559ce92ac38739e9f13988bef72e1514f",
+  "d91h31ki4bmdv8",
+  "pxgddeqduuuhxw",
+  "91fdcb24259dbcb1f3b22580adf6a25f172f4d6958998f490325ee23f682ca60",
   {
     logging: console.log,
     host: `ec2-54-224-194-214.compute-1.amazonaws.com`,
