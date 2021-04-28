a7f4e3aba459921ecc98ce97b7ad1c94ab4da7a7
Ashish Tiwari <ashish.tiwari@KAYADEV.COM>
2021-04-28 20:53:41 +0530

    Heroku DB details

--- src/dbconfig/db.js
+++ src/dbconfig/db.js
@@ -1,15 +1,22 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize("vizao", "postgres", "open", {
-  logging: console.log,
-  host: `localhost`,
-  dialect: `postgres`,
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-  define: {
-    timestamps: false,
-  },
-});
+module.exports = new Sequelize(
+  "dd7oskvf6vuls3",
+  "zmrcxwsomkcmhe",
+  "14900c88b9a37ea765cab9d97e19dcb559ce92ac38739e9f13988bef72e1514f",
+  {
+    logging: console.log,
+    host: `ec2-54-224-194-214.compute-1.amazonaws.com`,
+    dialect: `postgres`,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+    define: {
+      timestamps: false,
+    },
+  }
+);
+
+// postgres://zmrcxwsomkcmhe:14900c88b9a37ea765cab9d97e19dcb559ce92ac38739e9f13988bef72e1514f@ec2-54-224-194-214.compute-1.amazonaws.com:5432/dd7oskvf6vuls3
